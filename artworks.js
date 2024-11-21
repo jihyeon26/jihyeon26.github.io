205fc4d70cf58060874de170e4ab70b42ae63c41
Jihyeon Choung <jihyeon.choung@gmail.com>
2024-11-22 00:05:50 +0900

    artworks layout

--- artworks.js
+++ artworks.js
@@ -24,3 +24,22 @@ window.addEventListener('click', (event) => {
         modal.style.display = 'none';
     }
 });
+
+
+let lastScrollY = window.scrollY;
+
+function toggleNavOnScroll() {
+    const topnav = document.querySelector('.topnav');
+
+    if (window.scrollY > lastScrollY) {
+        // Scrolling down
+        topnav.classList.add('hidden');
+    } else {
+        // Scrolling up
+        topnav.classList.remove('hidden');
+    }
+
+    lastScrollY = window.scrollY;
+}
+
+window.addEventListener('scroll', toggleNavOnScroll);
