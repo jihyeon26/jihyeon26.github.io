91aa58398aa4f87d4c401f02fc5123755a95b0d9
Jihyeon Choung <jihyeon.choung@gmail.com>
2025-01-16 22:50:50 +0900

    edit media-scroll-machuilin

--- javascript/machuilin.js
+++ javascript/machuilin.js
@@ -14,4 +14,27 @@ function toggleNavOnScroll() {
     lastScrollY = window.scrollY;
 }
 
+
+
+// 미디어 쿼리에 따라 스크롤 이벤트를 활성화 또는 비활성화
+const mediaQuery = window.matchMedia('(max-width: 767px)'); // 작은 화면 조건
+function handleMediaQueryChange(e) {
+    const topnav = document.querySelector('.topnav');
+    if (e.matches) {
+        // 작은 화면에서는 스크롤 이벤트 제거하고 고정
+        topnav.classList.remove('hidden');
+        window.removeEventListener('scroll', toggleNavOnScroll);
+    } else {
+        // 큰 화면에서는 스크롤 이벤트 활성화
+        window.addEventListener('scroll', toggleNavOnScroll);
+    }
+}
+
+
+
+function toggleMenu() {
+  const topnav = document.querySelector('.topnav');
+  topnav.classList.toggle('show'); // 'show' 클래스 추가/제거
+}
+
 window.addEventListener('scroll', toggleNavOnScroll);
